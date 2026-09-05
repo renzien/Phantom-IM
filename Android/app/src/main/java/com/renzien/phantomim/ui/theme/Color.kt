a1e2902d6b80e56b497fe55644deb05f29c60111
Alif Rizki <alifrizki1011@gmail.com>
2026-09-05 19:39:46 +0700

    Update: Color.kt & Theme.kt

--- Android/app/src/main/java/com/renzien/phantomim/ui/theme/Color.kt
+++ Android/app/src/main/java/com/renzien/phantomim/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.renzien.phantomim.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PhantomRed = Color(0xFFC41001)
+val PhantomBlack = Color(0xFF080808)
+val PhantomWhite = Color(0xFFFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PhantomYellow = Color(0xFFEEFF28)
+val PhantomPink = Color(0xFFFF91C8)
+val PhantomBlue = Color(0xFF6FDAFA)
--- Android/app/src/main/java/com/renzien/phantomim/ui/theme/Theme.kt
+++ Android/app/src/main/java/com/renzien/phantomim/ui/theme/Theme.kt
@@ -1,57 +1,42 @@
 package com.renzien.phantomim.ui.theme
 
-import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+private val PhantomColorScheme = darkColorScheme(
+    primary = PhantomWhite,
+    onPrimary = PhantomBlack,
+    primaryContainer = PhantomBlack,
+    onPrimaryContainer = PhantomWhite,
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    // Secondary Color
+    secondary = PhantomYellow,
+    onSecondary = PhantomBlack,
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    // Third Color
+    tertiary = PhantomPink,
+    onTertiary = PhantomBlack,
+
+    // Background
+    background = PhantomRed,
+    onBackground = PhantomWhite,
+
+    // Surface
+    surface = PhantomBlack,
+    onSurface = PhantomWhite,
+    surfaceVariant = PhantomBlack,
+    onSurfaceVariant = PhantomWhite,
+
+    outline = PhantomWhite
 )
 
 @Composable
 fun PhantomIMTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
-) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
+){
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = PhantomColorScheme,
         typography = Typography,
         content = content
     )
