78be12a9fea4148b8b590c2df816f6029b908cb7
Alif Rizki <alifrizki1011@gmail.com>
2026-09-06 21:17:34 +0700

    Add: PhantomTextField

---
+++ Android/app/src/main/java/com/renzien/phantomim/ui/components/PhantomTextField.kt
@@ -0,0 +1,53 @@
+package com.renzien.phantomim.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.foundation.text.selection.TextSelectionColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.renzien.phantomim.R
+import com.renzien.phantomim.ui.theme.PhantomBlack
+import com.renzien.phantomim.ui.theme.PhantomIMTheme
+import com.renzien.phantomim.ui.theme.PhantomWhite
+import com.renzien.phantomim.ui.theme.PhantomYellow
+
+@Composable
+fun PhantomTextField(
+    state: TextFieldState,
+    label: String,
+    placeholder: String,
+    modifier: Modifier = Modifier,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default
+){
+    Column(modifier = modifier) {
+
+    }
+}
